85007006d4b8d79ad7a0dd7f86adbfa49c032909
Loong9274 <Loong9274@gmail.com>
2024-05-31 00:22:53 +0800

    modify window size to fit qrcode

--- test/testByMinifb.c
+++ test/testByMinifb.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-static const uint32_t g_width = 181;
+static uint32_t g_width = 181;
 static uint32_t *g_buffer = 0x0;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -27,9 +27,21 @@ int main()
 {
     uint32_t i, noise, carry, seed = 0xbeef;
 
-    struct mfb_window *window = mfb_open_ex("QRCode test", g_width*4, g_width*4, WF_ALWAYS_ON_TOP);
-    if (!window)
-        return 0;
+    QRCode *qr = QRCode_generate("HELLO WORLD",
+                               11,
+                               QRCODE_MODE_NOSPECIFY,
+                               QRCODE_ECC_NOSPECIFY,
+                               0,
+                               QRCODE_MASK_NOSPECIFY);
+    if (qr == NULL)
+    {
+        printf("qr return null");
+        return 1;
+    }
+
+    uint8_t qrWidth = qr->version*4+17;
+    g_width = qrWidth + 4;
+    g_width = g_width>32?g_width:33; // TODO issue minifb windows width min 164
 
     // init g_buffer
     g_buffer = (uint32_t *)calloc(4, g_width * g_width);
@@ -40,26 +52,16 @@ int main()
         g_buffer[g_width * j] = 0x00FFFFFF;
         g_buffer[g_width * j + (g_width - 1)] = 0x00FFFFFF;
     }
+    bitmap2UInt32(qr->data,
+    qrWidth,
+    qrWidth*qrWidth,
+    g_buffer+(g_width-qrWidth)/2*(g_width+1), // in the center
+    g_width,
+    0x00FFFFFF);
 
-    testQRCode();
-    QRCode *qr = QRCode_generate("HELLO WORLD",
-                               11,
-                               QRCODE_MODE_NOSPECIFY,
-                               QRCODE_ECC_NOSPECIFY,
-                               0,
-                               QRCODE_MASK_NOSPECIFY);
-    if (qr != NULL)
-    {
-        uint8_t qrWidth = qr->version*4+17;
-        bitmap2UInt32(qr->data,
-        qrWidth,
-        qrWidth*qrWidth,
-        g_buffer+(g_width-qrWidth)/2*(g_width+1), // in the center
-        g_width,
-        0x00FFFFFF);
-    }else{
-        printf("qr return null");
-    }
+    struct mfb_window *window = mfb_open_ex("QRCode test", g_width*4, g_width*4, WF_ALWAYS_ON_TOP);
+    if (!window)
+        return 0;
 
     mfb_update_state state;
     do
